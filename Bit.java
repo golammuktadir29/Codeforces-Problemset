f319cbf61cba6463fb4348abff93634d4aeedc41
golammuktadir29 <golammuktadir29@gmail.com>
2025-04-11 14:21:39 +0600

    (https://codeforces.com/problemset/problem/282/A)

---
+++ Bit.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+
+
+public class Bit {
+    
+    public static void main (String args[]){ 
+        int a =0;
+        
+        //int X=0; 
+        
+
+        System.out.println("Input the value ");
+        Scanner myobj = new Scanner(System.in);
+        int n = myobj.nextInt();
+		System.out.println();
+       // myobj.nextLine(); 
+		 //Scanner myobj1 = new Scanner(System.in);
+
+
+        String[] arr1 = new String[n]; 
+        for(int i=0;i<n;i++){ 
+           arr1[i]= myobj.nextLine();
+        }
+        for(int i=0;i<n;i++){
+            if(arr1[i].equals("X++") ||arr1[i].equals ("++X")){
+                //X++;
+                a += 1;
+
+
+            }
+            else if (arr1[i].equals("X--") ||arr1[i].equals ("--X") ) {
+                a -= 1;
+               // X--;
+            }
+        
+        }
+        System.out.println(  a);
+    
+    
+    
+
+    }
+
+  
+   
+
+}
